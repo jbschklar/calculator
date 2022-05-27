3c0119811d6557f556cc5e37f089524f4109e660
jbschklar <jbschklar@gmail.com>
2022-05-27 13:00:39 -0400

    created initial operate and mathematical functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const add = function (a, b) {
+	return a + b;
+};
+
+const subtract = function (a, b) {
+	return a - b;
+};
+
+const multiply = function (a, b) {
+	return a * b;
+};
+
+const divide = function (a, b) {
+	return a / b;
+};
+
+const operate = function (a, operator, b) {
+	if (operator === "+") return add(a, b);
+	if (operator === "-") return subtract(a, b);
+	if (operator === "*") return multiply(a, b);
+	if (operator === "/") return divide(a, b);
+};
+
+console.log(operate(10, "/", 5));
