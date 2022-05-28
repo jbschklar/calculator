f067106ac70c719a88f1b70134ed273f9e5fdbef
jbschklar <jbschklar@gmail.com>
2022-05-28 18:26:06 -0400

    added eventListeners and running total

--- calculator.js
+++ calculator.js
@@ -1,5 +1,18 @@
 "use strict";
 
+const display = document.querySelector(".display");
+const btnsNum = document.querySelectorAll(".btn-num");
+const btnsMain = document.querySelectorAll(".btn-main");
+const btnsOperator = document.querySelectorAll(".btn-operator");
+const btnsCommand = document.querySelectorAll(".btn-command");
+const btnEquals = document.querySelector(".equals");
+const btnClear = document.querySelector(".clear");
+display.textContent = 80085;
+let numCurr = [];
+let numSelectionA;
+let numSelectionB;
+let opSelection;
+let total;
 const add = function (a, b) {
 	return a + b;
 };
@@ -17,13 +30,48 @@ const divide = function (a, b) {
 };
 
 const operate = function (a, operator, b) {
+	if (typeof a !== "number" || typeof b !== "number") return;
+	if (
+		operator !== "+" &&
+		operator !== "-" &&
+		operator !== "*" &&
+		operator !== "/"
+	)
+		return;
 	if (operator === "+") return add(a, b);
 	if (operator === "-") return subtract(a, b);
 	if (operator === "*") return multiply(a, b);
 	if (operator === "/") return divide(a, b);
 };
 
-console.log(operate(10, "/", 5));
+const init = function () {
+	numCurr = [];
+	numSelectionA = undefined;
+	numSelectionB = undefined;
+	opSelection = undefined;
+	total = undefined;
+	display.textContent = 0;
+	console.log(numSelectionA, numSelectionB, opSelection, total);
+};
 
-const display = document.querySelector(".display");
-display.textContent = 80085;
+btnsMain.forEach((btn) => {
+	btn.addEventListener("click", function (e) {
+		let selection = e.target.textContent;
+
+		if (e.target.classList.contains("btn-num")) {
+			numCurr.push(+selection);
+			console.log(numCurr);
+		} else {
+			if (total) numSelectionA = total;
+			numSelectionA
+				? (numSelectionB = +numCurr.join(""))
+				: (numSelectionA = +numCurr.join(""));
+			if (e.target.classList.contains("btn-operator")) opSelection = selection;
+			numCurr = [];
+			total = operate(numSelectionA, opSelection, numSelectionB);
+			display.textContent = total;
+		}
+	});
+});
+
+btnClear.addEventListener("click", init);
